d2c99d963345808e4d27fe0ed548a39768668de5
Paras Jain <32563350+parasjain929@users.noreply.github.com>
2021-08-31 11:40:04 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,8 +32,7 @@ var SingleView = Backbone.View.extend({},
 /* * Flipbook View * */
 
 var aud= new Audio("page-flip-01a.mp3");
-$('#flipbook').on('turning', function(){
-aud.play();
+$('#flipbook').on('turning',{aud.play();
 } ); 
 var FlipbookView = SingleView.extend({
   el: '#flipbook',
